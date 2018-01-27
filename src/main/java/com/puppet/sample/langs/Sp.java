6cfe6926a2ee25d0256dfc379c90fc605ffa29cb
ipcrm <mcadorette@gmail.com>
2018-01-27 00:45:59 -0500

    remove extra junk

--- src/main/java/com/puppet/sample/langs/Sp.java
+++ src/main/java/com/puppet/sample/langs/Sp.java
@@ -1,18 +1,10 @@
 package com.puppet.sample.langs;
 
-/**
- * Hello world!
- *
- */
 public class Sp
 {
-
     public String Msg()
     {
         return "¡Hola Mundo!";
     }
 
-    public static void main( String[] args )
-    {
-    }
 }
