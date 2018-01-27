4c086957cbc108770bee8484a5aa7768e30ea233
ipcrm <mcadorette@gmail.com>
2018-01-27 00:10:57 -0500

    ini

---
+++ src/main/java/com/puppet/sample/langs/Sp.java
@@ -0,0 +1,13 @@
+package com.puppet.sample.langs;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sp
+{
+    public static String main( String[] args )
+    {
+        return "¡Hola Mundo!";
+    }
+}
